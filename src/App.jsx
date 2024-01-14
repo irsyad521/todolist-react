12c08fbaba776b1334ced0fa2dce754620f91fec
kali <arizonamarco521@gmail.com>
2024-01-14 20:57:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import List from './components/List';
+
+const App = () => {
+  return (
+    <div className="flex justify-center  bg-sky-400 min-w-full min-h-screen">
+      <List />
+    </div>
+  );
+};
+
+export default App;
