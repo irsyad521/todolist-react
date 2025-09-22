c9689c673e50d24cf9895dc9de21bea87a25ec1d
Muhammad Irsyad Al Khairy <117607680+irsyad521@users.noreply.github.com>
2025-09-22 17:32:56 +0700

    Update FormInput.jsx

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,28 +1,26 @@
 import { useState } from 'react';
 
-const FormInput = props => {
-  const { addTodo } = props;
+const FormInput = ({ addTodo }) => {
   const [value, setValue] = useState('');
-  const handlesubmit = e => {
+
+  const handleSubmit = e => {
     e.preventDefault();
-    if (value) {
-      addTodo(value);
-    }
+    if (value) addTodo(value);
     setValue('');
   };
 
   return (
-    <form className="flex gap-3 mb-10" onSubmit={handlesubmit}>
+    <form className="flex gap-3 mb-10" onSubmit={handleSubmit}>
       <input
         type="text"
-        className="w-full p-3 rounded-lg sm:text-xl bg-slate-300 dark:bg-gray-300 dark:text-gray-100 font-semibold"
         placeholder="Your Task"
         value={value}
         onChange={e => setValue(e.target.value)}
+        className="w-full p-3 rounded-lg sm:text-xl bg-slate-300 dark:bg-gray-300 dark:text-gray-100 font-semibold"
       />
       <button
         type="submit"
-        className="px-8 py-3 font-semibold border  dark:border-gray-100 dark:text-gray-100 bg-blue-700 rounded-md "
+        className="px-8 py-3 font-semibold border rounded-md bg-blue-700 dark:border-gray-100 dark:text-gray-100"
       >
         Add Task
       </button>
