4486430270e40c90a825b291ed881f203a9b2897
Muhammad Irsyad Al Khairy <117607680+irsyad521@users.noreply.github.com>
2025-09-22 17:44:03 +0700

    Update ListTodo.jsx

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -2,24 +2,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 
-const ListTodo = props => {
-  const { todo, deleteTodo, editTodo } = props;
+const ListTodo = ({ todo, deleteTodo, editTodo }) => {
   return (
     <div className="mt-2">
-      <div className="bg-slate-300  flex justify-between p-4 rounded-xl font-semibold text-xl">
+      <div className="bg-slate-300 flex justify-between p-4 rounded-xl font-semibold text-xl">
         <p className="w-4/5">{todo.task}</p>
-        <FontAwesomeIcon
-          icon={faDeleteLeft}
-          size="2x"
-          className="hover:cursor-pointer"
-          onClick={() => deleteTodo(todo.id)}
-        />
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          size="2x"
-          className="hover:cursor-pointer"
-          onClick={() => editTodo(todo.id)}
-        />
+
+        <div className="flex gap-4">
+          <FontAwesomeIcon
+            icon={faDeleteLeft}
+            size="2x"
+            className="hover:cursor-pointer"
+            onClick={() => deleteTodo(todo.id)}
+          />
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            size="2x"
+            className="hover:cursor-pointer"
+            onClick={() => editTodo(todo.id)}
+          />
+        </div>
       </div>
     </div>
   );
