374410478b215356aa7a508bfa578c9552027f69
Muhammad Irsyad Al Khairy <117607680+irsyad521@users.noreply.github.com>
2025-09-22 17:02:57 +0700

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const EditForm = props => {
-  const { editTask, task } = props;
-  const [value, setValue] = useState('');
+const EditForm = ({ editTask, task }) => {
+  const [inputValue, setInputValue] = useState("");
 
-  const handlesubmit = e => {
-    e.preventDefault();
-    if (value) {
-      editTask(task.id, value);
-      setValue('');
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValue.trim()) return;
+
+    editTask(task.id, inputValue.trim());
+    setInputValue("");
   };
+
   return (
-    <form className="flex gap-3 mb-10" onSubmit={handlesubmit}>
+    <form onSubmit={handleSubmit} className="flex gap-3 mb-10">
       <input
         type="text"
-        className="w-full p-3 rounded-lg sm:text-xl bg-slate-300 font-semibold"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         placeholder={task.task}
-        value={value}
-        onChange={e => setValue(e.target.value)}
+        className="w-full p-3 rounded-lg sm:text-xl bg-slate-300 font-semibold"
       />
       <button
         type="submit"
-        className="px-8 py-3 font-semibold border  dark:border-gray-100 dark:text-gray-100 bg-blue-700 rounded-md "
+        className="px-8 py-3 font-semibold border dark:border-gray-100 dark:text-gray-100 bg-blue-700 rounded-md"
       >
         Update
       </button>
     </form>
   );
 };
+
 export default EditForm;
