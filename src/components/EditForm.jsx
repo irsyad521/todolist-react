71f0cfa205ad77838ace8ead5c638c25ad153474
Muhammad Irsyad Al Khairy <117607680+irsyad521@users.noreply.github.com>
2025-09-22 17:21:25 +0700

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react';
 
-const EditForm = props => {
-  const { editTask, task } = props;
+const EditForm = ({ editTask, task }) => {
   const [value, setValue] = useState('');
 
-  const handlesubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (value) {
-      editTask(task.id, value);
-      setValue('');
-    }
+    if (!value) return;
+
+    editTask(task.id, value);
+    setValue('');
   };
+
   return (
-    <form className="flex gap-3 mb-10" onSubmit={handlesubmit}>
+    <form className="flex gap-3 mb-10" onSubmit={handleSubmit}>
       <input
         type="text"
-        className="w-full p-3 rounded-lg sm:text-xl bg-slate-300 font-semibold"
         placeholder={task.task}
         value={value}
-        onChange={e => setValue(e.target.value)}
+        onChange={(e) => setValue(e.target.value)}
+        className="w-full p-3 rounded-lg sm:text-xl bg-slate-300 font-semibold"
       />
       <button
         type="submit"
-        className="px-8 py-3 font-semibold border  dark:border-gray-100 dark:text-gray-100 bg-blue-700 rounded-md "
+        className="px-8 py-3 font-semibold border dark:border-gray-100 dark:text-gray-100 bg-blue-700 rounded-md"
       >
         Update
       </button>
     </form>
   );
 };
+
 export default EditForm;
